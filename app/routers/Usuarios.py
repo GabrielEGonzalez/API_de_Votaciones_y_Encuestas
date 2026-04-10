ac9b3dce3b5fe0051399bccf57d1523e6bcee989
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-09 22:33:13 -0500

    router de Usuario

--- app/routers/Usuarios.py
+++ app/routers/Usuarios.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter , Depends
 from sqlalchemy.orm import Session
 from configuracion_database import get_bd
+from app.schemas.Usuarios import CreaterUser
 
 userRouter = APIRouter(prefix="/v1")
 
 @userRouter.post("/user")
-async def creater_user(db:Session=Depends(get_bd())):
+async def creater_user(user:CreaterUser,db:Session=Depends(get_bd)):
     """ crear un nuevo usuario """
-    user = []
-    return await user
+    user
+    return user
 
 @userRouter.post("/login")
-async def login_user(db:Session=Depends(get_bd())):
+async def login_user(user:CreaterUser, db:Session=Depends(get_bd)):
     """ login de usuario que se le devolvera un id o un token"""
     return 
