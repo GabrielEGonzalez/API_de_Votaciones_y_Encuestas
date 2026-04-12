2396f7c5d89e630dee80890df61d9dc8ec27bc17
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-11 20:24:22 -0500

    linea de creacion de base de datos

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from app.routers.Usuarios import userRouter
+from configuracion_database import Base , engine
 
 app = FastAPI(
     title="API de encuesta y votaciones",
     version="1.0.0",
     description="Esta es una API de votaciones y encuestas en vivo. Será usada para manejar datos de los usuarios y es una API RESTful."
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/root", response_class=HTMLResponse)
 async def root():
