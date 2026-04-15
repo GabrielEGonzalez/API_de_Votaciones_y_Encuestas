8fd3ab0f3beee983d62dcd98ba8231196806b336
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-14 22:09:04 -0500

    repositorio de usuario

---
+++ app/repositories/usuario_repo.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from ..model.Usuario import Usuarios
+from passlib.hash import pbkdf2_sha256
+
+class userRepositorio():
+    def __init__(self,db:Session):
+        self.db = db
+    
+    def createUser(self,user=Usuarios):
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+    
+    def getUserByID(self,password:str,correo:str): #metodo no funcionando logica incloclusa
+        user_id = self.db.query(select(Usuarios).where(Usuarios.correo == correo))
