f89f6d382804bce490bb98121414e32e025633fb
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-15 21:00:55 -0500

    dependencia de logica de clase

---
+++ dependencies.py
@@ -0,0 +1,10 @@
+from configuracion_database import get_bd
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from .app.repositories.usuario_repo import userRepositorio
+from .app.services.usuario_service import usuarioService
+
+
+def get_conexion(bd:Session=Depends(get_bd))-> usuarioService:
+    repo = userRepositorio(bd)
+    return usuarioService(repo)
