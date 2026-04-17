d5f2566318c4332e9b3bd0237461029c2f9b3654
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-16 20:27:41 -0500

    metodo de servicio usuario

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,9 +1,23 @@
 from ..repositories.usuario_repo import userRepositorio
 from app.schemas.Usuarios import CreaterUser
+from app.model.Usuario import Usuarios
+from passlib.hash import pbkdf2_sha256
+import bcrypt
 
 class usuarioService():
     def __init__(self,repo:userRepositorio):
         self.repo = repo
     
     def create_user(self,user:CreaterUser):
-        return self.repo.createUser(user)
+        passhash = pbkdf2_sha256.hash(user.password)
+        modelo_user = Usuarios(nombre=user.nombre,correo=user.correo,password=passhash)
+        return self.repo.createUser(modelo_user)
+    
+    def login_usuario(self):
+        pass
+    
+    def obtener_usuario_id(sefl):
+        pass
+    
+    def obtener_encuestas_usuario(self):
+        pass
