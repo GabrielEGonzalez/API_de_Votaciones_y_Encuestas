c0459163aa1fc7f2c0f11f8eae3b39562b6ede91
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-17 23:04:58 -0500

    feat(repository): add user repository methods with SQLAlchemy

--- app/repositories/usuario_repo.py
+++ app/repositories/usuario_repo.py
@@ -6,17 +6,20 @@ class userRepositorio():
     def __init__(self,db:Session):
         self.db = db
     
-    def createUser(self,user=Usuarios):
+    def createUser(self,user:Usuarios):
         self.db.add(user)
         self.db.commit()
         self.db.refresh(user)
         return user
     
-    def getUserByEmail(self,correo:str): #metodo no funcionando logica incloclusa
-        user_id = self.db.query(select(Usuarios).where(Usuarios.correo == correo))
+    def getUserByEmail(self,correo:str)-> Usuarios | None :
+        usuario_correo = self.db.execute(select(Usuarios).where(Usuarios.correo == correo)).scalars().first()
+        return usuario_correo
     
-    def getUserById(self,id:int):
-        pass
+    def getUserById(self,id:int)-> Usuarios | None :
+        user_id = self.db.execute(select(Usuarios).where(Usuarios.id == id)).scalars().first()
+        return user_id
+    
     
     def obtener_encuestas_por_usuario_id(self):
         pass
