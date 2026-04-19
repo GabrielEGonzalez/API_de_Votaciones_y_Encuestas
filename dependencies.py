d82013ccceca3a303e9f937cabc423eb0f70da9e
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-19 15:13:13 -0500

    metodo implemnetacion de conexion del servicio y repositorio

--- dependencies.py
+++ dependencies.py
@@ -1,8 +1,8 @@
 from configuracion_database import get_bd
 from sqlalchemy.orm import Session
 from fastapi import Depends
-from .app.repositories.usuario_repo import userRepositorio
-from .app.services.usuario_service import usuarioService
+from app.repositories.usuario_repo import userRepositorio
+from app.services.usuario_service import usuarioService
 
 
 def get_conexion(bd:Session=Depends(get_bd))-> usuarioService:
