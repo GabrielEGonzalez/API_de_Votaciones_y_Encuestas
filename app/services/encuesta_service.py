d633c235621bfc760a7c2d0bb1b8f7c07fe797d5
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-28 22:22:21 -0500

    feat: estructura de la clase de servicio

---
+++ app/services/encuesta_service.py
@@ -0,0 +1,49 @@
+"""
+Lógica fuerte aquí:
+
+crear_encuesta_con_opciones
+listar_encuestas
+obtener_encuesta_por_id
+obtener_resultados
+obtener_top_encuestas
+obtener_encuestas_sin_votos
+cambiar_estado_encuesta
+eliminar_encuesta
+
+👉 Validaciones:
+
+usuario creador existe
+encuesta activa/inactiva
+manejar creación de opciones automáticamente
+"""
+
+from ..repositories.encuesta_repo import EncuestaRepository
+from .app.schemas.Encuestas import createEncuesta , outEncuesta
+class EncuestaService():
+
+    def __init__(self,encuestaRepo:EncuestaRepository) -> None:
+        self.encuesta_repo = encuestaRepo
+
+    def crear_encuesta_con_opciones(self):
+        pass
+
+    def listar_encuestas(self):
+        pass
+
+    def obtener_encuesta_por_id(self):
+        pass
+
+    def obtener_resultados(self):
+        pass
+
+    def obtener_top_encuestas(self):
+        pass
+
+    def obtener_encuestas_sin_votos(self):
+        pass
+
+    def cambiar_estado_encuesta(self):
+        pass
+
+    def eliminar_encuesta(self):
+        pass
