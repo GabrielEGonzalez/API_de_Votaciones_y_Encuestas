1df36da7f64ee362babe2c5528f74f1b64d2b2bf
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-04-28 22:21:20 -0500

    nuevo cambio en la estructura del esquema de salida

--- app/schemas/Encuestas.py
+++ app/schemas/Encuestas.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel,Field 
-from pydantic.validate_call_decorator import validate_call
 from datetime import date
-from schemas.Opciones import Opcion , OpcionOut
-from typing import List
+from ..schemas.Opciones import Opcion , OpcionOut
 
 class createEncuesta(BaseModel):
     titulo: str = Field(title="titulo de encuesta")
@@ -10,8 +8,16 @@ class createEncuesta(BaseModel):
     opciones: list[Opcion] = Field(description="lista de opciones de encuesta")
     creador_id: int = Field(title="id del usuario",gt=0)
 
-class outEncuesta(createEncuesta):
-    id:int
-    opciones: List[OpcionOut]
-    fecha_creacion: date = Field(title="fecha de encuesta",description="fecha de creacion de encuesta")
-    activa: bool = Field(title="estado de encuesta")
+class outEncuesta(BaseModel):
+    id: int
+    titulo: str
+    descripcion: str
+    opciones: list[OpcionOut]
+    creador_id: int
+    fecha_creacion: date = Field(
+        title="fecha de encuesta",
+        description="fecha de creacion de encuesta"
+    )
+    activa: bool = Field(
+        title="estado de encuesta"
+    )
