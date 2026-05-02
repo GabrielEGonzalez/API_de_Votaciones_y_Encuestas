7d2e6eb8ba9b7f6e2b93b1024b6b40f06baf1d89
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-01 21:38:42 -0500

    archivo de servicio de votos

---
+++ app/services/voto_service.py
@@ -0,0 +1,14 @@
+"""
+Aquí está la lógica crítica del sistema:
+
+registrar_voto
+validar_usuario_existe
+validar_opcion_existe
+verificar_usuario_ya_voto
+obtener_encuesta_por_opcion
+
+👉 ⚠️ Esta parte es CLAVE:
+
+evitar votos duplicados
+validar integridad
+"""
