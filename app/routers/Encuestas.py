9aacf9b99ad8725deca49b4b0495a84f92e5398b
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-03 21:23:38 -0500

    feat:creacion de endpoint de encuesta con implementacion de servicios

--- app/routers/Encuestas.py
+++ app/routers/Encuestas.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Depends, Path
 from typing import Annotated
+from dependencies import get_conexion_encuesta
+from ..services.encuesta_service import EncuestaService
+from ..schemas.Encuestas import createEncuesta
+from fastapi.security import OAuth2PasswordBearer
+
+from app.services import encuesta_service
 
 """"
 POST /encuestas
@@ -15,13 +21,19 @@ DELETE /encuestas/{id}
 encuesta_router = APIRouter(prefix="/v1")
 
 @encuesta_router.post('/encuestas')
-async def crear_encuesta():
-    pass
+async def crear_encuesta(encuesta:createEncuesta,service:EncuestaService=Depends(get_conexion_encuesta)):
+    token = ""
+    encuesta_creacion = service.crear_encuesta_con_opciones(encuesta,token)
+    return encuesta_creacion
 
 @encuesta_router.get('/encuestas')
-async def obtener_encuesta():
-    pass
+async def obtener_encuesta(service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.listar_encuestas()
 
 @encuesta_router.get('/encuestas/{id}')
-async def obtener_encuesta_id(id:Annotated[int,Path()]):
-    pass
+async def obtener_encuesta_id(id:Annotated[int,Path()],service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.obtener_encuesta_por_id(id)
+
+@encuesta_router.get('/encuesta/top')
+async def ontener_encuesta_top(service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.obtener_top_encuestas()
