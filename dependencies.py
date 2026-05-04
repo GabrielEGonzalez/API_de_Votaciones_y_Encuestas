4248eb2e93462e721922e1119ac64914b870cca6
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-03 21:22:40 -0500

    feat:nueva dependecia de servicio de encuesta

--- dependencies.py
+++ dependencies.py
@@ -3,8 +3,13 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from app.repositories.usuario_repo import userRepositorio
 from app.services.usuario_service import usuarioService
-
+from app.services.encuesta_service import EncuestaService
+from app.repositories.encuesta_repo import EncuestaRepository
 
 def get_conexion(bd:Session=Depends(get_bd))-> usuarioService:
     repo = userRepositorio(bd)
     return usuarioService(repo)
+
+def get_conexion_encuesta(db:Session=Depends(get_bd))-> EncuestaService:
+    repo = EncuestaRepository(db)
+    return EncuestaService(repo)
