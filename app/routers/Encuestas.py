94252cfc155d0483474e209c1e607ce097b78d9c
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-05 22:49:54 -0500

    feat: nuevos endpoint con funcinalidades

--- app/routers/Encuestas.py
+++ app/routers/Encuestas.py
@@ -2,25 +2,11 @@ from fastapi import APIRouter, Depends, Path
 from typing import Annotated
 from dependencies import get_conexion_encuesta
 from ..services.encuesta_service import EncuestaService
-from ..schemas.Encuestas import createEncuesta
-from fastapi.security import OAuth2PasswordBearer
-
-from app.services import encuesta_service
-
-""""
-POST /encuestas
-GET /encuestas
-GET /encuestas/{id}
-GET /encuestas/{id}/resultados
-GET /encuestas/top
-GET /encuestas/sin-votos
-PATCH /encuestas/{id}/estado
-DELETE /encuestas/{id}
-"""
+from ..schemas.Encuestas import createEncuesta , outEncuesta
 
 encuesta_router = APIRouter(prefix="/v1")
 
-@encuesta_router.post('/encuestas')
+@encuesta_router.post('/encuestas',response_model=outEncuesta)
 async def crear_encuesta(encuesta:createEncuesta,service:EncuestaService=Depends(get_conexion_encuesta)):
     token = ""
     encuesta_creacion = service.crear_encuesta_con_opciones(encuesta,token)
@@ -34,6 +20,21 @@ async def obtener_encuesta(service:EncuestaService=Depends(get_conexion_encuesta
 async def obtener_encuesta_id(id:Annotated[int,Path()],service:EncuestaService=Depends(get_conexion_encuesta)):
     return service.obtener_encuesta_por_id(id)
 
-@encuesta_router.get('/encuesta/top')
-async def ontener_encuesta_top(service:EncuestaService=Depends(get_conexion_encuesta)):
+@encuesta_router.get('/encuestas/top')
+async def obtener_encuesta_top(service:EncuestaService=Depends(get_conexion_encuesta)):
     return service.obtener_top_encuestas()
+
+@encuesta_router.get('/encuestas/sin-votos')
+async def obtener_encuesta_sin_votos(service:EncuestaService=Depends(get_conexion_encuesta)):   return service.obtener_encuestas_sin_votos()
+
+@encuesta_router.patch('/encuestas/{id}/estado')
+async def estado_encuesta(id:Annotated[int,Path(gt=0)],service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.cambiar_estado_encuesta()
+
+@encuesta_router.delete('/encuestas/{id}')
+async def eliminar_encuesta(id:Annotated[int,Path(gt=0)],service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.eliminar_encuesta(id)
+
+@encuesta_router.get('/encuestas/resultados')
+async def obtener_resultados(service:EncuestaService=Depends(get_conexion_encuesta)):
+    return service.obtener_resultados()
