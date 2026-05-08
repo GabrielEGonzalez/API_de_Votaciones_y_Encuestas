f125c046e64a94abe468fc57c3b33f79a8b14459
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-07 23:07:39 -0500

    feat: creacion de estructuras y metodo de la clase reporitorio de votos

--- app/repositories/voto_repo.py
+++ app/repositories/voto_repo.py
@@ -9,3 +9,28 @@ contar_votos_por_encuesta
 verificar_opcion_existe
 obtener_encuesta_id_por_opcion
 """
+
+from ..model.Votos import Votos
+from sqlalchemy.orm import Session
+from sqlalchemy import select 
+
+class VotoRepository():
+
+    def __init__(self,db:Session):
+        self.db = db
+
+    def crear_voto(self):
+        pass
+
+    def buscar_voto_por_usuario_y_encuesta(self):
+        pass
+
+    def contar_votos_por_encuesta(self):
+        pass
+
+    def verificar_opcion_existe(self):
+        pass
+
+    def obtener_encuesta_id_por_opcion(self):
+        pass
+
