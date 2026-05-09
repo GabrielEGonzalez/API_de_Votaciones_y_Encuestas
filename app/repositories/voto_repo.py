0c229d721c5855a7a840bc5ef8c722d39e099e5d
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-08 22:42:48 -0500

    feat: creacion de consultas de motodo de repositorio de votos

--- app/repositories/voto_repo.py
+++ app/repositories/voto_repo.py
@@ -10,17 +10,26 @@ verificar_opcion_existe
 obtener_encuesta_id_por_opcion
 """
 
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 from ..model.Votos import Votos
 from sqlalchemy.orm import Session
-from sqlalchemy import select 
+from ..model.Opciones import Opciones
 
 class VotoRepository():
 
     def __init__(self,db:Session):
         self.db = db
 
-    def crear_voto(self):
-        pass
+    def crear_voto(self,votos:Votos):
+        try:
+            self.db.add(votos)
+            self.db.commit()
+            self.db.refresh(votos)
+
+            return votos
+        except SQLAlchemyError as e:
+            raise(e)
 
     def buscar_voto_por_usuario_y_encuesta(self):
         pass
@@ -28,8 +37,13 @@ class VotoRepository():
     def contar_votos_por_encuesta(self):
         pass
 
-    def verificar_opcion_existe(self):
-        pass
+    def verificar_opcion_existe(self,id:int):
+
+        try:
+            opcion = self.db.execute(select(Opciones).where(Opciones.id == id)).scalars().first()         
+            return opcion
+        except SQLAlchemyError as e:
+            raise(e)
 
     def obtener_encuesta_id_por_opcion(self):
         pass
