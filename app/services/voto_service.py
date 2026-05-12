415bf221ab7ff4f8a160bebecaf31526a44fe2ed
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2026-05-11 20:25:21 -0500

    feat:nueva estructura de clase votos service

--- app/services/voto_service.py
+++ app/services/voto_service.py
@@ -12,3 +12,40 @@ obtener_encuesta_por_opcion
 evitar votos duplicados
 validar integridad
 """
+
+class VotoService:
+    def __init__(self, voto_repository, encuesta_repository, usuario_repository):
+        self.voto_repo = voto_repository
+        self.encuesta_repo = encuesta_repository
+        self.usuario_repo = usuario_repository
+
+    def registrar_voto(self, usuario_id: int, opcion_id: int):
+        """
+        Orquestador principal que coordina las validaciones 
+        y la creación final del voto.
+        """
+        pass
+
+    def validar_usuario_existe(self, usuario_id: int):
+        """
+        Verifica en el repositorio de usuarios si el ID es válido.
+        """
+        pass
+
+    def validar_opcion_existe(self, opcion_id: int):
+        """
+        Verifica en el repositorio de encuestas si la opción existe.
+        """
+        pass
+
+    def verificar_usuario_ya_voto(self, usuario_id: int, encuesta_id: int):
+        """
+        Consulta si existe un registro previo para evitar duplicidad.
+        """
+        pass
+
+    def obtener_encuesta_por_opcion(self, opcion_id: int):
+        """
+        Recupera la entidad Encuesta vinculada a la opción seleccionada.
+        """
+        pass
